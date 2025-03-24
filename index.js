3a7202e48dfdc8475f749a58887e28d123f519f4
Rami_Aljaber <ramialjaber2004@gmail.com>
2025-03-25 02:22:07 +0300

    user interation updates

--- index.js
+++ index.js
@@ -1,24 +1,66 @@
+const letters=document.querySelectorAll('.letter-field');
+const loadingDiv=document.querySelector('.info-bar'); 
+const MAX_WORD_LENGTH=5;
 
-/**
- * if the user click on .bage he will be directed to last place he was in 
- * we have 6 row each row is seperated form the other row
- * in each row we have 5 div (column) and when they all fill in the letters we cant add more
- * we can use backspace to delete from the last and rewrite 
- * just the letters and backspace and enter are working 
- * if we press enter the word would supmited (we can press enter only when there are 5 letters)
- * when we press enter and the word submitted we will make a for loop to check for the letters 
- * if the letter is in his write palce div background will be green 
- * if the letter is in the word but not in his write place background will be yellow
- * if the letter not in the word backgrond well be gray
- * when i press enter it will go to the next line
- */
 
-function init(){
-    document.querySelector(".bage").addEventListener("click",function(event){
-        alert("hi");
-    })
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
+  }
+
+async function init() {
+    let currentGuess=``;
+    let currentRow=0;
+
+    const response=await fetch("https://words.dev-apis.com/word-of-the-day?random=1");
+    const responseObject= await response.json();
+    const word = responseObject.word.toUpperCase();
+    console.log(word);
+
+        function letterInsert(letter){
+            if(currentGuess.length<MAX_WORD_LENGTH)
+                currentGuess+=letter;
+            else
+                currentGuess=currentGuess.substring(0,currentGuess.length-1)+letter;
+            
+        
+            letters[(MAX_WORD_LENGTH*currentRow)+currentGuess.length-1].innerText=letter;
+        }
+        
+
+
+
+        function submit(){
+            if(currentGuess.length!== MAX_WORD_LENGTH)
+                return ;
+            
+            currentRow++;
+            currentGuess=``;
+        }
+        
+
+
+        function backSpace(){
+            currentGuess=currentGuess.substring(0,currentGuess.length-1);
+            letters[(MAX_WORD_LENGTH*currentRow)+currentGuess.length].innerText="";
+        }
+    
+
+
+
+    document.addEventListener('keydown',function handleKeyPress(event){
+        const action = event.key;
+        if(action === 'Enter'){
+            submit();
+        }
+        else if(action === 'Backspace'){
+            backSpace();
+        }
+        else if(isLetter(action)){
+            letterInsert(action.toUpperCase());
+        }
+    });
+
 }
-
 
 
 init();
